dbd5b1a7958e0a202c1de7325a209fb197ff5d7c
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-05-16 19:41:41 -0500

    Adaptation to python3 and tf2

--- train.py
+++ train.py
@@ -1,23 +1,28 @@
-import sys
+import sys, os
 import numpy as np
 import tensorflow as tf
-
+import logging
 from models.JMOD2 import JMOD2
 from lib.trainer import Trainer
 from config import get_config
 from lib.utils import prepare_dirs
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 config = None
 
-def main(_):
+def main():
 	# Log dirs
 	prepare_dirs(config)
-	# Model JMOD2
+
+    # RNG initialization
 	rng = np.random.RandomState(config.random_seed)
-	tf.set_random_seed(config.random_seed)
+	tf.random.set_seed(config.random_seed)
+
+	# Instantiate the JMOD2 model
 	model = JMOD2(config)
+
 	# Prepare trainer
 	trainer = Trainer(config, model, rng)
+
 	# train
 	if config.resume_training:
 		trainer.resume_training()
@@ -25,5 +30,5 @@ def main(_):
 		trainer.train()
 
 if __name__ == "__main__":
-	config, unparsed = get_config()
-	tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    config, unparsed = get_config()
+    main()
